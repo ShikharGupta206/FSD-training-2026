35c65f8c851a4d9f4fdfc671a818cf7ed46c23d0
ShikharGupta206 <1176shg@gmail.com>
2026-01-30 12:49:45 +0530

    jslearn

---
+++ ch1.js
@@ -0,0 +1,21 @@
+// console.log("hello");
+// console.error("0");
+// console.warn("1");
+
+// console.table({a:1,b:2});
+// console.time("test")
+// for(let i=0;i<10000;i++){}
+// console.timeEnd("test");
+
+// console.group("My group");
+// console.log("Inside the group");
+// console.groupEnd();
+
+// console.count("click")
+// console.count("click")
+// console.count("click")
+// console.count("click")
+// console.count("click")
+// console.count("click")
+// console.count("click")
+console.dir(document.body);
